4a96667af4558d75c21f54d89820a523188d80fe
hakonw <hakonwar@gmail.com>
2019-09-18 00:14:51 +0200

    Add exercise 2 question 2 test file

---
+++ 03p/oving3-question2-binaryintervalsearch.jl
@@ -0,0 +1,29 @@
+
+
+
+## Du skal implementere denne funksjonen 
+function binaryintervalsearch(x, delta, coordinate)
+    # todo
+end
+
+
+
+
+
+### Tests ###
+# Disse testene blir kjør når du kjører filen
+# Du trenger ikke å endre noe her, men du kan eksperimentere! 
+
+printstyled("\n\n---------------\nKjører tester!!\n---------------\n"; color = :magenta)
+
+using Test
+@testset "Basic tests" begin
+    @test binaryintervalsearch([1 2; 2 3; 3 0; 4 0; 5 1], 1.5, 1) == (2, 4)
+    @test binaryintervalsearch([1 0; 2 0; 2 0; 3 0; 4 0; 5 0; 5 0], 1, 1) == (2, 5)
+    @test binaryintervalsearch([1 2; 2 0; 3 3; 4 4], 0.25, 1) == (-1, -1) # Tom mengde
+
+end
+
+println("\nFungerte alt? Prøv å kjør koden i inginious!")
+println("Husk at disse testene ikke alltid sjekker alle edge-cases")
+println("---------------------------------------------------------\n\n\n\n\n")
